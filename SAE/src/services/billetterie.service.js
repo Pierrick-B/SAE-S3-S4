5f68b9770d664bf47e9cb21812a88ea85aa95bd9
Damien Bretteville <damien.bretteville@edu.univ-fcomte.fr>
2025-10-17 16:02:14 +0200

    Billetterie update

--- SAE/src/services/billetterie.service.js
+++ SAE/src/services/billetterie.service.js
@@ -7,20 +7,49 @@ class BilletterieService {
     }
 
     async acheterBillet(nom) {
-        const billet = billets.find(b => b.nom === nom);
+        const allBillets = Object.values(billets).flat();
+        const billet = allBillets.find(b => b.nom === nom);
+
         if (!billet || billet.quantite <= 0) {
             return { error: 1, message: "Billet épuisé" };
         }
-        billet.quantite--;
-        let billetsModifies = [{ nom: billet.nom, quantite: billet.quantite }];
-        if (billet.jours && billet.jours.length > 1) {
-            billets
-                .filter(b => b.jours?.length === 1 && billet.jours.some(j => b.jours[0] === j))
-                .forEach(b1j => {
-                    if (b1j.quantite > 0) b1j.quantite--;
+
+        let billetsModifies = [];
+
+        // Décrémentation du billet acheté
+        if (billet.jours.length === 1) {
+            billet.quantite--;
+            billetsModifies.push({ nom: billet.nom, quantite: billet.quantite });
+        } else {
+            // Décrémenter les billets 1 jour correspondants
+            billet.jours.forEach(jour => {
+                const b1j = allBillets.find(b => b.jours.length === 1 && b.jours[0] === jour);
+                if (b1j && b1j.quantite > 0) {
+                    b1j.quantite--;
                     billetsModifies.push({ nom: b1j.nom, quantite: b1j.quantite });
-                });
+                }
+            });
         }
+
+        allBillets.forEach(b => {
+            if (b.jours.length > 1) {
+                const quantiteMin = Math.min(
+                    ...b.jours.map(jour => {
+                        const b1j = allBillets.find(bj => bj.jours.length === 1 && bj.jours[0] === jour);
+                        return b1j ? b1j.quantite : 0;
+                    })
+                );
+                b.quantite = quantiteMin;
+                billetsModifies.push({ nom: b.nom, quantite: b.quantite });
+            }
+        });
+
+        billetsModifies = Object.values(
+            billetsModifies.reduce((acc, curr) => {
+                acc[curr.nom] = curr;
+                return acc;
+            }, {})
+        );
 
         return { error: 0, data: billetsModifies };
     }
--- SAE/src/stores/billetterie.js
+++ SAE/src/stores/billetterie.js
@@ -4,23 +4,25 @@ import BilletterieService from "@/services/billetterie.service.js";
 
 export const useBilletterieStore = defineStore('billetterie', {
     state: () => ({
-        billets: [],
+        billets: {},
     }),
     actions: {
         async fetchBilletterie() {
             let response = await BilletterieService.getBilletterie()
             if (response.error === 0) {
-                this.billets = response.data
+                this.billets = JSON.parse(JSON.stringify(response.data))
             }
         },
         async acheterBillet(nom) {
             let response = await BilletterieService.acheterBillet(nom)
             if (response.error === 0) {
                 response.data.forEach(update => {
-                    const billet = this.billets.find(b => b.nom === update.nom)
-                    if (billet) billet.quantite = update.quantite
+                    for (const key of Object.keys(this.billets)) {
+                        const billet = this.billets[key].find(b => b.nom === update.nom)
+                        if (billet) billet.quantite = update.quantite
+                    }
                 })
-                this.billets = [...this.billets]
+                this.billets = { ...this.billets }
             }
         }
     },

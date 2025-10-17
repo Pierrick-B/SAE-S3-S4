55a2272cc6844f909b4fb982320dad6437e3471b
Leenknegt Noa <noa.leenknegt@edu.univ-fcomte.fr>
2025-10-17 09:07:58 +0200

    Ajout Main.vue

---
+++ SAE/src/components/Main.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useLangStore } from "@/stores/langStore.js";
+import { storeToRefs } from "pinia";
+
+const langStore = useLangStore();
+const { currentTranslations } = storeToRefs(langStore);
+</script>
+
+<template>
+  <p>POPCON25</p>
+</template>

2410a8920bebb1353d3b479d64074a6a9f18ca72
Matheo <matheo.bonnet5@gmail.com>
2025-12-07 21:17:58 +0100

    ajout map interieur

--- SAE/src/datasource/stands.js
+++ SAE/src/datasource/stands.js
@@ -2,7 +2,7 @@ const stands = [
     {
         id: "HALL-A-01",
         label: "Neo Arcade",
-        zone: "Hall A",
+        zone: "extérieur",
         dimensions: {
             largeur_m: 10,
             profondeur_m: 10
@@ -49,13 +49,13 @@ const stands = [
         map_infos: {
             type: "polygon",
             coords: [[[757.4817379765262,658.5364012816275],[804.1093659869573,665.8313000828934],[812.2148090994748,619.6302743415425],[766.4830458541114,612.1220744057366],[757.4817379765262,658.5364012816275]]],
-            color: "00ff00"
+            color: "#00ff00"
         }
     },
     {
         id: "HALL-A-02",
         label: "Indie Station",
-        zone: "Hall A",
+        zone: "extérieur",
         dimensions: {
             largeur_m: 8,
             profondeur_m: 6
@@ -108,13 +108,13 @@ const stands = [
         map_infos: {
             type: "polygon",
             coords: [[[636.5771408872839,562.4068018051612],[646.0731792352947,512.1833100979047],[697.2462747773521,519.9911638507135],[688.277794115342,571.0587478555709],[636.5771408872839,562.4068018051612]]],
-            color: "ffaa00"
+            color: "#ffaa00"
         }
     },
     {
         id: "HALL-B-01",
         label: "Street Bites",
-        zone: "Hall B",
+        zone: "extérieur",
         dimensions: {
             largeur_m: 5,
             profondeur_m: 2
@@ -147,13 +147,13 @@ const stands = [
         map_infos: {
             type: "polygon",
             coords: [[[631.1626528063092,585.3273152746675],[676.0412115135127,592.7075345297495],[667.6371468126393,637.5007727831368],[623.0998699206997,630.3338546625947],[631.1626528063092,585.3273152746675]]],
-            color: "ff0066"
+            color: "#ff0066"
         }
     },
     {
         id: "HALL-B-02",
         label: "Makers Corner",
-        zone: "Hall B",
+        zone: "extérieur",
         dimensions: {
             largeur_m: 6,
             profondeur_m: 4
@@ -218,7 +218,7 @@ const stands = [
     {
         id: "HALL-C-02",
         label: "Creative Alley",
-        zone: "Hall C",
+        zone: "extérieur",
         dimensions: {
             largeur_m: 4,
             profondeur_m: 2
@@ -229,6 +229,86 @@ const stands = [
         status: "available",
         occupant: null,
         requests: []
+    },
+    {
+        id: "GYM-INT-001",
+        label: "Stands intérieurs",
+        zone: "extérieur",
+        dimensions: {
+            largeur_m: 50,
+            profondeur_m: 40
+        },
+        surface_m2: 2000,
+        tarif_emplacement_eur: 0,
+        infrastructures: ["electricite", "internet", "climatisation"],
+        status: "occupied",
+        occupant: null,
+        requests: [],
+        map_infos: {
+            type: "polygon",
+            coords: [[[835.58646101972, 517.8595438986426], [844.0742691171718, 469.2099121205654], [1034.739421745296, 511.2349131884364], [998.0969331294675, 555.1230916435529], [835.58646101972, 517.8595438986426]]],
+            color: "#9C27B0"
+        }
+    },
+    {
+        id: "INT-STAND-01",
+        label: "Stand Intérieur 1",
+        zone: "intérieur",
+        dimensions: {
+            largeur_m: 6,
+            profondeur_m: 4
+        },
+        surface_m2: 24,
+        tarif_emplacement_eur: 200,
+        infrastructures: ["electricite"],
+        status: "available",
+        occupant: null,
+        requests: [],
+        map_infos: {
+            type: "polygon",
+            coords: [[[99.56640625000011, 880.65625], [513.2578125000001, 882.05859375], [516.0625000000001, 746.03125], [100.96875000000011, 743.2265625], [99.56640625000011, 880.65625]]],
+            color: "#FF6B6B"
+        }
+    },
+    {
+        id: "INT-STAND-02",
+        label: "Stand Intérieur 2",
+        zone: "intérieur",
+        dimensions: {
+            largeur_m: 6,
+            profondeur_m: 4
+        },
+        surface_m2: 24,
+        tarif_emplacement_eur: 200,
+        infrastructures: ["electricite"],
+        status: "available",
+        occupant: null,
+        requests: [],
+        map_infos: {
+            type: "polygon",
+            coords: [[[521.6718750000001, 875.7480468749999], [524.4765625000001, 749.5371093749999], [835.7968750000001, 746.7324218749999], [835.7968750000001, 878.5527343749999], [521.6718750000001, 875.7480468749999]]],
+            color: "#4ECDC4"
+        }
+    },
+    {
+        id: "INT-STAND-03",
+        label: "Stand Intérieur 3",
+        zone: "intérieur",
+        dimensions: {
+            largeur_m: 8,
+            profondeur_m: 6
+        },
+        surface_m2: 48,
+        tarif_emplacement_eur: 300,
+        infrastructures: ["electricite", "internet"],
+        status: "available",
+        occupant: null,
+        requests: [],
+        map_infos: {
+            type: "polygon",
+            coords: [[[841.4062500000001, 877.1503906249999], [841.4062500000001, 748.1347656249999], [1336.43359375, 749.5371093749999], [1335.03125, 884.1621093749999], [841.4062500000001, 877.1503906249999]]],
+            color: "#FFE66D"
+        }
     }
 ];
 

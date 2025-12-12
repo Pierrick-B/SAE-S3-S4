8cd4a18c82d9cb1caf26e9935d70e25ec004a5ad
Noa Leenknegt <noa.leenknegt@du.univ-fcomte.fr>
2025-12-12 10:31:16 +0100

    fix data panier

--- SAE/src/datasource/data_panier.js
+++ SAE/src/datasource/data_panier.js
@@ -1,17 +1,5 @@
 const panierData = {
   items: [
-    {
-      id: "b-jeu-jeudi",
-      nom: "Billet 1 jour - jeudi",
-      prix: 10,
-      quantite: 0
-    },
-    {
-      id: "b-jeu-vendredi",
-      nom: "Billet 1 jour - vendredi",
-      prix: 10,
-      quantite: 0
-    }
   ]
 };
 
